3f77b4b18b6f4aee5f8de96d2e81d8ea37f07610
silverframe <ibercombie@gmail.com>
2017-07-30 14:24:15 +0800

    first commit

---
+++ device-where/device-whereTests/device_whereTests.swift
@@ -0,0 +1,36 @@
+//
+//  device_whereTests.swift
+//  device-whereTests
+//
+//  Created by Stefanie Seah on 30/7/17.
+//  Copyright © 2017 Stefanie Seah. All rights reserved.
+//
+
+import XCTest
+@testable import device_where
+
+class device_whereTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
